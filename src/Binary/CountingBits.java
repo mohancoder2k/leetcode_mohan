283031a2fbb76290bc91af944f557a2099b1d75a
mohancoder2k <mohanbalu2k5@gmail.com>
2024-04-22 19:57:37 +0530

    Binary-CountingBits

---
+++ src/Binary/CountingBits.java
@@ -0,0 +1,32 @@
+package Binary;
+
+	public class CountingBits {
+		public static void main(String[] args) {
+			int n=5;
+			String Binary = decimal(n);
+			int count = Onecount(Binary);
+			System.out.println(Binary);
+			System.out.println(count);
+		}
+		public static String decimal(int n) {
+	
+			StringBuilder binary = new StringBuilder();
+			while(n>0) {
+				int rem = n%2;
+				binary.insert(0,rem);
+				n/=2;
+			}
+			
+			return binary.toString() ;
+		}
+	public static int Onecount(String Binary) {
+		int count = 0;
+		for(int i=0;i<Binary.length();i++) {
+			if(Binary.charAt(i)=='1') {
+				count++;
+			}
+		}
+		return count;
+	}
+
+}
