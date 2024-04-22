568b930ff0e091b56f5f2e61b1d73570bb54959c
mohancoder2k <mohanbalu2k5@gmail.com>
2024-04-22 20:32:15 +0530

    updated

--- src/Binary/CountingBits.java
+++ src/Binary/CountingBits.java
@@ -1,32 +1,57 @@
 package Binary;
 
-	public class CountingBits {
-		public static void main(String[] args) {
-			int n=5;
-			String Binary = decimal(n);
-			int count = Onecount(Binary);
-			System.out.println(Binary);
-			System.out.println(count);
+import java.util.ArrayList;
+
+public class CountingBits {
+	public static void main(String[] args) {
+		int n=5;
+		
+		// System.out.println(Binary);
+		int[] array= ArrayResult(n); 
+		
+		for(int i=0;i<array.length;i++) {
+			System.out.println(array[i]);
 		}
-		public static String decimal(int n) {
+		
+	}
 	
-			StringBuilder binary = new StringBuilder();
-			while(n>0) {
-				int rem = n%2;
-				binary.insert(0,rem);
-				n/=2;
-			}
-			
-			return binary.toString() ;
+	// My Method to convert decimal into BINARY
+	public static String decimal(int n) {
+		if(n==0) {
+			return "0";
 		}
+		StringBuilder binary = new StringBuilder();
+		while(n>0) {
+			int rem = n%2;
+			binary.insert(0, rem);
+			n/=2;
+		}
+		return binary.toString();
+	}
+	// Method to count the number of 1's digit in Number
 	public static int Onecount(String Binary) {
-		int count = 0;
+		int flag=0;
 		for(int i=0;i<Binary.length();i++) {
 			if(Binary.charAt(i)=='1') {
-				count++;
+				flag++;
 			}
 		}
-		return count;
+		return flag;
 	}
-
+	// Method to return the required array
+	public static int[] ArrayResult(int n) {
+		ArrayList<Integer> keyList = new ArrayList();
+		for(int i=0;i<=n;i++) {
+			String Binary = decimal(i);
+			int count = Onecount(Binary);
+			keyList.add(count);
+		}
+		int[] key = new int[keyList.size()];
+		for(int i=0;i<keyList.size();i++) {
+			key[i]=keyList.get(i);
+		}
+		return key;
+		
+	}
+	
 }
